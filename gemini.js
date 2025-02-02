0302856334ff3fd234952c80ccbabeeac4571fad
Balaji_Asish <balajiashish02@gmail.com>
2025-02-02 14:34:01 +0530

    add gemini api

---
+++ gemini.js
@@ -0,0 +1,17 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+async function giveContent(text) {
+  const systemPrompt =
+    "you are an AI assistant , Give summary of this json file.";
+  const prompt = text;
+  const result = await model.generateContent(systemPrompt + prompt);
+  console.log(result.response.text());
+}
+
+export default giveContent;
